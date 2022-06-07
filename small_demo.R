44e8cfc30e1ecc0432c80d6fb8a931e3467aa744
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-06-07 09:51:00 -0500

    Added sim.master and added step to small_demo.R to run FINEMAP.

--- small_demo.R
+++ small_demo.R
@@ -22,4 +22,16 @@ fit1 <- susie_rss(z,Rin,n = 800,min_abs_corr = 0.1,refine = FALSE,
 cat("SuSiE CSs:\n")
 print(fit1$sets[c("cs","purity")])
 
-# Run FINEMAP.
+# Run FINEMAP... not working!
+p   <- length(b)
+dat <- data.frame(rsid       = 1:p,
+                  chromosome = rep(1,p),
+                  position   = rep(1,p),
+                  allele1    = rep("A",p),
+                  allele2    = rep("C",p),
+                  maf        = round(maf,digits = 6),
+                  beta       = round(bhat,digits = 6),
+                  se         = round(shat,digits = 6))
+write.table(dat,"sim.z",quote = FALSE,col.names = TRUE,row.names = FALSE)
+system(paste("./finemap_v1.4.1_x86_64 --sss --log --in-files sim.master",
+             "--n-causal-snps 5"))
