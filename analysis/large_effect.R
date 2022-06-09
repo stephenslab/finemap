7af3892991629521646cf3e08822e119fe2691d5
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-06-08 23:44:19 -0500

    Working on large_effect demo.

--- analysis/large_effect.R
+++ analysis/large_effect.R
@@ -1,19 +1,3 @@
-library(data.table)
-library(susieR)
-set.seed(1)
-
-# Load the summary data.
-dat1 <- readRDS("small_data_11.rds")
-dat2 <- readRDS("small_data_11_sim_gaussian_pve_n_8.rds")
-dat3 <- readRDS("small_data_11_sim_gaussian_pve_n_8_get_sumstats_n_1.rds")
-b    <- drop(dat2$meta$true_coef)
-maf  <- dat1$maf$in_sample
-bhat <- dat3$sumstats$bhat
-shat <- dat3$sumstats$shat
-z    <- bhat/shat
-cat("True causal SNPs:\n")
-print(which(b != 0))
-
 # Run susie.
 n <- 800
 ldfile <- "small_data_11_sim_gaussian_pve_n_8_get_sumstats_n_1.ld_sample_n_file.in_n.ld"
@@ -21,8 +5,6 @@ ldfile <- "small_data_11_sim_gaussian_pve_n_8_get_sumstats_n_1.ld_sample_n_file.
 R <- as.matrix(fread(ldfile))
 fit <- susie_rss(z,R,n = 800,min_abs_corr = 0.1,refine = FALSE,
                  verbose = TRUE)
-cat("SuSiE CSs:\n")
-print(fit$sets[c("cs","purity")])
 
 # Run FINEMAP.
 p   <- length(b)
